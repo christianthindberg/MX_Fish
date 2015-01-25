83ee006426b18a93a6b495c7a96ddebfa41778e5
Christian Thindberg <christian@thindberg.no>
2015-01-25 23:07:48 +0100

    Complete example

--- app.js
+++ app.js
@@ -6,9 +6,23 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
-
+/*
 io.on('connection', function(socket){
     console.log('a user connected');
+});
+*/
+/*
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+    });
+});
+*/
+
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+    });
 });
 
 http.listen(process.env.PORT || 3000, function(){
