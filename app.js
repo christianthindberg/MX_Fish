14410edd9321a08a323a971d573cd8cdb2733d42
Christian Thindberg <christian@thindberg.no>
2015-01-25 23:20:45 +0100

    Complete example

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile('./public//index.html');
 });
 
 /*
@@ -23,7 +23,6 @@ io.on('connection', function(socket){
     socket.on('chat message', function(msg){
         io.emit('chat message', msg);
     });
-});
 
 http.listen(process.env.PORT || 3000, function(){
     console.log(process.env.PORT);
